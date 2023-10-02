fa0b12a0132524d12f2fbb49948b6c88efa9b11c
MuhammadAslam-K <aslamka.2k3@gmail.com>
2023-10-02 06:22:48 +0530

    created the project structer

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import Express, { Request, Response } from 'express';
+import * as dotenv from "dotenv"
+import cors from 'cors'
+
+import connect from './config/mongoDB';
+
+dotenv.config()
+
+const port = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL
+const app = Express()
+app.use(cors())
+app.use(Express.json())
+
+const allowedOrigins = ['http://localhost:5173'];
+app.use(
+    cors({
+        origin: function (origin: any, callback: any) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        },
+        credentials: true,
+    })
+);
+
+
+MONGO_URL ? connect(MONGO_URL).then(() => {
+
+    app.listen(port, () => console.log(`server started at http://localhost:${port}`))
+}) : console.log("Cannot access the url from env");
